c6aa03cd98a75d7f746e1cae07619622c3eb0742
Jose Quinones <quinonesji@knights.ucf.edu>
2021-01-13 19:27:45 -0500

    init

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper mapper;
+
+        public MakesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            this.mapper = mapper;
+        }
+        
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+            var makes = await  _context.Makes.Include(m => m.Models).ToListAsync();
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
+        }
+    }
+}
---
+++ Migrations/20210113214419_AddFeature.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class AddFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+        }
+    }
+}
