3d5c1a5f6c790bb224066fe223c3ad44585d5def
Jose Quinones <quinonesji@knights.ucf.edu>
2021-01-22 21:45:58 -0500

    populate form

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,7 +1,9 @@
+import * as _ from 'underscore'; 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { VehicleService } from 'src/services/vehicle.service';
 import { forkJoin } from 'rxjs';
+import { SaveVehicle, Vehicle } from 'src/models/vehicle';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -12,15 +14,27 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   features: any[]; 
-  vehicle: any = {
+  vehicle: SaveVehicle = {
+    id: 0,
+    makeId: 0,
+    modelId: 0,
+    isRegistered: false,
     features: [],
-    contact: {}
+    contact: {
+      name: '',
+      email: '',
+      phone: '',
+    }
   };
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private vehicleService: VehicleService) { }
+    private vehicleService: VehicleService) {
+      route.params.subscribe(p => {
+        this.vehicle.id = +p['id'];
+      });
+     }
 
   ngOnInit() {
 
@@ -35,8 +49,11 @@ export class VehicleFormComponent implements OnInit {
       forkJoin(sources).subscribe((data: any[]) => {
         this.makes = data[0];
         this.features = data[1];
-        if (this.vehicle.id)
-        this.vehicle = data[2];
+        if (this.vehicle.id) {
+          this.setVehicle(data[2]);
+          this.populateModels();
+        }
+          
     }, err => {
       if (err.status == 404)
         this.router.navigate(['/']);
@@ -48,6 +65,15 @@ export class VehicleFormComponent implements OnInit {
 
     
   }
+
+  private setVehicle(v: Vehicle) {
+    this.vehicle.id = v.id;
+    this.vehicle.makeId = v.make.id;
+    this.vehicle.modelId = v.model.id;
+    this.vehicle.isRegistered = v.isRegistered;
+    this.vehicle.contact = v.contact;
+    this.vehicle.features = _.pluck(v.features, 'id');
+  } 
 
   onFeatureToggle(featureId, $event) {
     if ($event.target.checked)
@@ -64,8 +90,12 @@ export class VehicleFormComponent implements OnInit {
   }
 
   onMakeChange() {
+    this.populateModels();
+    delete this.vehicle.modelId;
+  }
+
+  private populateModels() {
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
-    delete this.vehicle.modelId;
   }
 }
