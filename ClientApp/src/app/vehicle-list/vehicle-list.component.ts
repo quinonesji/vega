8d7fca0ca499652bda385faecabc7b6652929771
Jose Quinones <quinonesji@knights.ucf.edu>
2021-01-24 18:09:09 -0500

    pass sort params to server and add sort icons

--- ClientApp/src/app/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/vehicle-list/vehicle-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
   makes: KeyValuePair[];
-  filter: any = {};
+  query: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
@@ -24,13 +24,23 @@ export class VehicleListComponent implements OnInit {
   }
 
   private populateVehicles() {
-    this.vehicleService.getVehicles(this.filter)
+    this.vehicleService.getVehicles(this.query)
       .subscribe((vehicles: Vehicle[]) => this.vehicles = vehicles);
   }
 
   resetFilter() {
-    this.filter = {};
+    this.query = {};
     this.onFilterChange();
+  }
+
+  sortBy(columnName) {
+    if (this.query.sortBy === columnName) {
+      this.query.isSortAscending = !this.query.isSortAscending; ; 
+    } else {
+      this.query.sortBy = columnName;
+      this.query.isSortAscending = true;
+    }
+    this.populateVehicles();
   }
 
 } 
